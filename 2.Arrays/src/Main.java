74a01c1273ad691e67aa28b40724cd850ca609aa
Bekalu <bekpostgrad@gmail.com>
2021-06-08 15:00:28 +0200

    Arrays

---
+++ 2.Arrays/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String [] args){
+        //System.out.print("Hello");
+
+        int[] numbers = new int[5];
+
+        // initialize an array
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        System.out.println(Arrays.toString(numbers));
+
+
+        int [] sequence = {9,3,1,5,6};
+        Arrays.sort(sequence);
+        System.out.println(Arrays.toString(sequence));
+
+        // MD Arrays
+
+        boolean [][] truthTable_2d = new boolean[2][3];
+
+        truthTable_2d[0][0] = true;
+
+        System.out.println(Arrays.deepToString(truthTable_2d)+"\n");
+
+
+        // MD Names
+
+        String [][] namesList = new String[4][2];
+        namesList[0][0] = "firstName";
+        namesList[0][1] = "lastName";
+
+        namesList[1][0] = "Bekalu";
+        namesList[1][1] = "Tadesse";
+
+        System.out.println(Arrays.deepToString(namesList)+"\n");
+
+        // MD names initialization way-2
+
+        String [][] namesList2 = {{"firstName","lastName"},{"Bekalu", "Tadesse"}};
+
+        System.out.println(Arrays.deepToString(namesList2));
+    }
+}
