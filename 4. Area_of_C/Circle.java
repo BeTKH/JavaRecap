b2157df54210d74cd266521cda9cd97cdb2b699b
Bekalu <bekpostgrad@gmail.com>
2021-06-10 16:11:34 +0200

    area of C

---
+++ 4. Area_of_C/Circle.java
@@ -0,0 +1,8 @@
+public class Circle {
+    public static void main(String [] args){
+        double radius = 20;
+        double area = radius * radius * 3.14;
+
+        System.out.println(area);
+    }
+}
