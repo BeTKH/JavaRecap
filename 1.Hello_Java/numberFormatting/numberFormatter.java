f9821b4c940ecc697ff71e03da9b536994cf8cb6
Bekalu <bekpostgrad@gmail.com>
2021-06-12 13:14:25 +0200

    the overflow effcet on int

--- 1.Hello_Java/numberFormatting/numberFormatter.java
+++ 1.Hello_Java/numberFormatting/numberFormatter.java
@@ -1,9 +1,9 @@
 import java.text.NumberFormat;
 
 public class numberFormatter {
-    public static void main(String[] args) {
-        NumberFormat currencyValue = NumberFormat.getCurrencyInstance();
+    public static void main(String[] args) {NumberFormat currencyValue = NumberFormat.getCurrencyInstance();
         String result = currencyValue.format(1234567.891);
         System.out.println(result);
+
     }
 }
---
+++ 3.casting/OverflowEffect.java
@@ -0,0 +1,27 @@
+public class OverflowEffect {
+    public static void main(String[] args) {
+
+        // calc using int
+        int maxIntValue = (int)(Math.pow(2,31))-1;
+
+        int overFlowInt = (int)(Math.pow(2,31))+10;
+
+        System.out.println("\n\n");
+        
+        System.out.println("value (2^31)-1 in int is: "+ maxIntValue);
+
+        System.out.println("value (2^31)+10 in int is: "+ overFlowInt + "\n=>This number became negative because of the overflow effect! \n\n");
+
+
+        // calc using double
+
+        double maxOfInt = (double) (Math.pow(2,31))-1;
+        double overOfInt = (double) (Math.pow(2,31))+10;
+
+        System.out.println("value (2^31)-1 in double is: "+ maxOfInt);
+
+        System.out.println("value (2^31)+10 in double is: "+ overOfInt);
+
+
+    }
+}
