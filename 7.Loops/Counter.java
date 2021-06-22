648e1008873aec8d50c07c4c41fbc90651196978
Bekalue <bekalue.tkh@gmail.com>
2021-06-23 00:19:56 +0200

    update

---
+++ 7.Loops/Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+    public static void main(String[] args) {
+        int setCounter = 0;
+        while (setCounter < 100){
+                    System.out.println(setCounter+"<--Hello Java-->");
+                    setCounter ++;
+        }
+    }
+}
