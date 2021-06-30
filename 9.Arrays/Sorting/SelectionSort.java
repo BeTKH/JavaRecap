db3de8f63c2371e38360cb371a1e0320274fa37c
Bekalue <bekalue.tkh@gmail.com>
2021-06-30 19:48:09 +0200

    sorting

---
+++ 9.Arrays/Sorting/SelectionSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] list_  = {2,9,5,4,8,1,6};
+
+        System.out.println(Arrays.toString(list_));
+
+    }
+
+    public static int[] SortingMeth( int[] array_){
+
+        for( int k = 0; k < array_.length ; k++){
+            if ( array_[k])
+        }
+    }
+}
