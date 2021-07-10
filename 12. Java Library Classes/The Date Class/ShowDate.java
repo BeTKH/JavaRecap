138dfc306a7e1a898cbf3d5b6f243cd9edae3323
Bekalue <bekalue.tkh@gmail.com>
2021-07-10 14:07:55 +0200

    using java.util Date class

---
+++ 12. Java Library Classes/The Date Class/ShowDate.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class ShowDate {
+    public static void main(String[] args) {
+
+        Date todate = new Date();
+        System.out.println("Today is : "+todate.toString());
+
+    }
+}
