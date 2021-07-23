fe725bb0f4272a9675882517c01960d59080dbb8
Bekalue <bekalue.tkh@gmail.com>
2021-07-23 13:04:56 +0200

    isIsolated number check

---
+++ 8.Methods/Problems/isIsolated.java
@@ -0,0 +1,15 @@
+public class isIsolated {
+
+    public static void main(String[] args) {
+
+        isIsolatedNum(163);
+
+    }
+
+    public static void isIsolatedNum( long Number){
+
+        long result = Number * Number;
+        System.out.println(result);
+
+    }
+}
