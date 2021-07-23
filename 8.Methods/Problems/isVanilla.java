9cd8ceca6b7fa6ed178065aa6c89a4962fd1a5f9
Bekalue <bekalue.tkh@gmail.com>
2021-07-23 20:43:59 +0200

    is vanilla array update

--- 8.Methods/Problems/isVanilla.java
+++ 8.Methods/Problems/isVanilla.java
@@ -6,12 +6,14 @@ public class isVanilla {
 
 
 
-        System.out.println(isVani(new int[]  {1, 1, 11, 1111, 1111111}));
-        System.out.println(isVani(new int[]  {11, 101, 1111, 11111}));
+
         System.out.println(isVani(new int[]  {1}));
         System.out.println(isVani(new int[]  {11, 22, 13, 34, 125}));
         System.out.println(isVani(new int[]  {9, 999, 99999, -9999}));
         System.out.println(isVani(new int[]  { }));
+        System.out.println();
+        System.out.println(isVani(new int[]  {1, 1, 11, 1111, 1111111}));
+        System.out.println(isVani(new int[]  {11, 101, 1111, 11111}));
 
     }
 
@@ -19,25 +21,40 @@ public class isVanilla {
         // set a flag -- assume the list is vanilla initially
         int isVanilla = 1;
 
-        // concatenate the numbers to form a string
-        String stringNumber = "";
-        for (int i = 0 ; i < list.length ; i++){
-            stringNumber += Integer.toString(list[i]);
-        }
+        if( list.length == 0){ // if its empty set .. its automatically vanilla
+            isVanilla = 1;
+        }else{
 
-        // the first character
-        char prevDigit = stringNumber.charAt(0);
-        for (int i = 0; i < stringNumber.length(); i++) {
+            // concatenate the numbers to form a string
+            String stringNumber = "";
+            for (int i = 0 ; i < list.length ; i++){
 
-            // if any of the next umber is different from the previous -- is NOT VANILLA -- change the flag -- and break the loop
-            if (stringNumber.charAt(i) != prevDigit){
-                isVanilla = 0;
-                break;
+                // if the number is negative ... the sign shall not matter
+                if (list[i] < 0){
+                    stringNumber += Integer.toString(-1 * list[i]);
+                }else{
+                    stringNumber += Integer.toString(list[i]);
+                }
             }
 
-            // update the prev value
-            prevDigit = stringNumber.charAt(i);
+            //System.out.println(stringNumber);
+
+            // the first character
+            char prevDigit = stringNumber.charAt(0);
+            for (int i = 0; i < stringNumber.length(); i++) {
+
+                // if any of the next umber is different from the previous -- is NOT VANILLA -- change the flag -- and break the loop
+                if (stringNumber.charAt(i) != prevDigit){
+                    isVanilla = 0;
+                    break;
+                }
+
+                // update the prev value
+                prevDigit = stringNumber.charAt(i);
+            }
+
         }
+
 
         return isVanilla;
     }
