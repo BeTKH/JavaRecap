7c383a5dbc2e5d98dc3cc6b15070199410c1e6f9
Bekalue <bekalue.tkh@gmail.com>
2021-08-12 16:57:51 +0200

    update date

--- 12. Java Library Classes/Date_Class/ShowDate.java
+++ 12. Java Library Classes/Date_Class/ShowDate.java
@@ -4,6 +4,7 @@ import java.util.Date;
 public class ShowDate {
     public static void main(String[] args) {
 
+
         Date todate = new Date();
         System.out.println("Today is : "+todate.toString());
 
