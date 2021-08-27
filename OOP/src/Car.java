836792f271fb9b23e795e736a9f5446601ba14b5
Bekalue <bekalue.tkh@gmail.com>
2021-08-27 14:57:55 +0200

    initializing an object using a constructor

--- OOP/src/Car.java
+++ OOP/src/Car.java
@@ -4,12 +4,21 @@ public class Car {
     public String color;
     public int numberOfSeats;
 
+    // a method that displays object state
     public void displayCarState(){
 
         System.out.println("The model of the car is : "+model);
         System.out.println("The color of the car is : "+color);
         System.out.println("The number of the seats the car has is : "+numberOfSeats);
 
+    }
+
+    // define a constructor that can initialize an object
+
+    Car(String mod, String  col, int seats){
+        model = mod ;
+        color = col;
+        numberOfSeats = seats ;
     }
 
 }
