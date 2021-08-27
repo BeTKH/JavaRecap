8d4195ae17167e981a9fe7cd4deec0cfb0fd72a6
Bekalue <bekalue.tkh@gmail.com>
2021-08-27 15:58:08 +0200

    inheritance update

--- OOP/2. Inheritance/src/InheritanceMain.java
+++ OOP/2. Inheritance/src/InheritanceMain.java
@@ -3,7 +3,9 @@ public class InheritanceMain {
     public static void main(String[] args) {
 
         Ferari ferariCar1 = new Ferari();
-        
+        ferariCar1.color = "Red";
+
+        System.out.println(ferariCar1.color);
 
     }
 }
