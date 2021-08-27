e9113432ed3bc2fd7b506d41fde7de7ad364edda
Bekalue <bekalue.tkh@gmail.com>
2021-08-27 14:15:02 +0200

    oop recap

---
+++ OOP/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+
+    public String model;
+    public String color;
+    public int numberOfSeats;
+
+    public void displayCarState(){
+
+        System.out.println("The model of the car is : "+model);
+        System.out.println("The color of the car is : "+color);
+        System.out.println("The number of the seats the car has is : "+numberOfSeats);
+
+    }
+
+}
