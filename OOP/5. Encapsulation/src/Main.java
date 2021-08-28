807b5637368055055eae0c04183c75d81ba394a0
Bekalue <bekalue.tkh@gmail.com>
2021-08-28 12:56:21 +0200

    encapsulation in Java

---
+++ OOP/5. Encapsulation/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student abebe = new Student();
+
+        abebe.setName("Abebe");
+        abebe.setAge(18);
+
+        System.out.println("Name of the studnet is : "+abebe.getName());
+        System.out.println("Age of a student is : "+abebe.getAge());
+
+    }
+}
---
+++ OOP/5. Encapsulation/src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+
+    private String name;
+    private int age;
+
+    // create a method that sets a name
+    public void setName( String newName){
+        name = newName;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge( int newAge){
+        age = newAge;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+}
