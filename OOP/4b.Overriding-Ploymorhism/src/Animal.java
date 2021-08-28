659a38c5e4829151d9e58dacaa763d7637391f62
Bekalue <bekalue.tkh@gmail.com>
2021-08-28 12:53:23 +0200

    plolymorphism implementation Java

---
+++ OOP/4b.Overriding-Ploymorhism/src/Animal.java
@@ -0,0 +1,7 @@
+public class Animal {
+
+    public void makeSound(){
+        System.out.println("Just make sound..");
+        System.out.println("This is the Animal class, your blue print! LOL ;))");
+    }
+}
---
+++ OOP/4b.Overriding-Ploymorhism/src/Cat.java
@@ -0,0 +1,8 @@
+public class Cat extends Dog{
+
+    public void makeSound(){
+        System.out.println("Mew! Mew! ...I have changed it Dog!");
+        System.out.println("This is Cat class ;)");
+    }
+
+}
